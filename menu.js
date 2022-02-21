a4ba6bab25163bf4a997bba51e0fd5b7914edbdb
iamstillcath <catherine2lucia@gmail.com>
2022-02-21 13:17:04 +0100

    resturant

---
+++ menu.js
@@ -0,0 +1,362 @@
+let items = [
+
+    {
+        type: "shakes",
+        name: "lights",
+        image: "https://pallybiscuits.com/wp-content/uploads/2018/09/digestive-500x300-500x300.jpg",
+        price: "$1.53",
+        description: "cookies,cabins,crackers,parleG, speedy,fish-biscuit cookies,cabins,crackers,parleG, speedy,fish-biscuit cookies,cabins,crackers,parleG, speedy,fish-biscuit"
+    },
+    {
+        type: "shakes",
+        name: "banana shakes",
+        image: "https://media.istockphoto.com/photos/milk-shakes-in-plastic-cups-and-straws-on-a-dark-wooden-table-picture-id1311373994?b=1&k=20&m=1311373994&s=170667a&w=0&h=jBCnbjNhWueI5JJNV1Qagba2ek8PtGM9iZjmcPaSJPI=",
+        price: "$1.08",
+        description: "blended milk with blended milk with banana blended milk with blended milk with banana blended milk with blended milk with banana"
+    },
+
+
+    {
+        type: "launch",
+        name: "samosas",
+        image: "https://image.shutterstock.com/image-photo/small-chops-combo-260nw-792009511.jpg",
+        price: "$6.92",
+        description: "fried samosa, puff-puffs,gizzards,springrolls fried samosa, puff-puffs,gizzards,springrolls fried samosa, puff-puffs,gizzards,springrolls fried samosa, puff-puffs,gizzards,springrolls"
+    },
+
+    {
+        type: "launch",
+        name: "bread",
+        image: "https://media.istockphoto.com/photos/heap-of-bread-picture-id995038782?b=1&k=20&m=995038782&s=170667a&w=0&h=DhnB0WJOySvxTwwCOeUagMmDCGFEykDxZsCgmkOSIZM=",
+        price: "$3.02",
+        description: " bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread brown bread"
+    },
+
+
+    {
+        type: "shakes",
+        name: "non-alcholic",
+        image: "https://img.lovepik.com/photo/40031/2791.jpg_wh300.jpg",
+        price: "$2.04",
+        description: "milkshakes juice, wine,smoothies,water,enegry drinks,milkshakes juice, wine,smoothies,water,enegry drinks,milkshakes juice, wine,smoothies,water,enegry drinks,milkshakes"
+    },
+
+    {
+        type: "dinner",
+        name: "soups",
+        image: "https://image.shutterstock.com/image-photo/vegetable-soup-on-white-plate-260nw-1443832184.jpg",
+        price: "$1.03",
+        description: "nsala,onugbu,ogbona,white soup egusi,ewedu,afang,ifokore,onugbu,ogbona,white egusi,ewedu,afang,ifokore,efor,nsala,onugbu,ogbona"
+    },
+    {
+        type: "dinner",
+        name: "swallows",
+        image: "https://www.radianthealthmag.com/wp-content/uploads/2015/01/Eba-11.jpg",
+        price: "$5.76",
+        description: "amala,eba,pounded yam,semovita,fufu,dawa dawa,starch, tuwo amala,eba,pounded yam,semovita,fufu,dawa dawa,starch, tuwo",
+    },
+
+
+    {
+        type: "breakfast",
+        name: "meats",
+        image: "https://media.istockphoto.com/photos/balanced-diet-food-background-picture-id1151799593?k=20&m=1151799593&s=612x612&w=0&h=jRs5hyc8u1QZ1hqL5IrPQTwyXqG_i2KGyYDlN7IbCzY=",
+        price: "$1.09",
+        description: "pork,beef,chicken,sharki,roundabout,gizzards, meat,pork,beef,chicken,sharki,roundabout,gizzards, meat,pork,beef,chicken,sharki,roundabout,gizzards"
+    },
+
+    {
+        type: "breakfast",
+        name: "rice",
+        image: "https://media.istockphoto.com/photos/table-top-view-of-indian-food-picture-id1212329362?b=1&k=20&m=1212329362&s=170667a&w=0&h=hIJRzsC-B1IfoghcbdxbNfKL-f9ma_4Z-JWzs1JU8gE=",
+        price: "$2.07",
+        description: "white rice white rice white rice,white rice white rice white rice,white rice white rice white rice,white rice white rice white rice,white rice white rice white rice,white rice white rice"
+    },
+
+
+    {
+        type: "launch",
+        price: "$2.65",
+        name: "snack",
+        image: "https://mindbodygreen-res.cloudinary.com/image/upload/c_fill,g_auto,w_480,h_322,q_auto,f_auto,fl_lossy/org/v7933wggqkpwpmeri.jpg",
+        description: " dorritos, chips,meatballs donuts,meatpies,fishpie,eggrolls,meatballs donuts,meatpies,fishpie,,eggrolls,sausageroll,meatball,chips, plantains"
+    },
+
+    {
+        type: "shakes",
+        name: "alcoholic drinks",
+        image: "https://media.istockphoto.com/photos/five-cocktails-in-hands-joined-in-celebratory-toast-picture-id1303977605?b=1&k=20&m=1303977605&s=170667a&w=0&h=P5Kio_7K3oGKmheY6QYyeb6I6AdaAI2avS1VYSzeknU=",
+        price: "$1.28",
+        description: "baron,azul,rozy,moet.whisky,bst,hennessy,jackdaniels baron,azul,rozy,moet,whisky,hennessy,jackdaniels baron,azul,rozy,moet,gin,whisky,bst,hennessy,jackdaniels"
+    },
+
+    {
+        type: "breakfast",
+        name: "veg",
+        image: "https://media.istockphoto.com/photos/variety-of-fresh-organic-vegetables-and-fruits-in-the-garden-picture-id1280856062?b=1&k=20&m=1280856062&s=170667a&w=0&h=wQu-c2ZjzeCBkAGEj69xpF611lx1i_xim48vOCj_Dw0=",
+        price: "$3.09",
+        description: ",vegetables,watermelons,grapes great carrots,apples,lemons,vegetables,watermelons,grapes great carrots,apples,lemons,vegetables,watermelons,grapes",
+    },
+
+    {
+        type: "dinner",
+        name: "scrambled eggs",
+        image: "https://www.asweetpeachef.com/wp-content/uploads/2014/09/how-to-make-the-best-scrambled-eggs-4.jpg",
+        price: "$2.09",
+        description: " onions and eggs fried with tomates,  onions and eggs fried with tomates,  onions and eggs fried with tomates,  onions and eggs fried with tomates,  onions and eggs fried with tomates,  onions and eggs"
+    },
+    {
+        type: "launch",
+        price: "$2.01",
+        name: "flour",
+        image: "https://media.istockphoto.com/photos/field-of-different-types-of-donuts-picture-id465529983?b=1&k=20&m=465529983&s=170667a&w=0&h=wZ403DEEzskTZ7GFDTya4apixOM-iRoVYd7CIOPvcOI=",
+        description: "donuts,meatpies,fishpie,sausageroll,meatballs, donuts,meatpies,fishpie,sausageroll,meatballs eggrolls, donuts,meatpies,fishpie,sausageroll,meatballs",
+    },
+]
+
+
+
+
+let pages
+let content = document.getElementById('content');
+
+for (let i = 0; i < items.length; i++) {
+    let div = document.createElement('div');
+    let names = document.createElement('h1');
+    let food = document.createElement('h4');
+    let amount = document.createElement('h2');
+    let title = document.createElement('h4');
+    let images = document.createElement('img');
+    let div2 = document.createElement('div')
+    let line = document.createElement('hr');
+
+
+
+    food.textContent = items[i].name;
+    food.style.marginLeft = "48%";
+    food.style.marginBottom = "12%"
+    food.style.color = " rgb(151, 140, 170)";
+    food.style.fontSize = "24px";
+    food.style.textTransform = "capitalize";
+    amount.textContent = items[i].price;
+    amount.style.marginLeft = "78%";
+    amount.style.marginTop = "-16%";
+    amount.style.color = "chocolate"
+    title.textContent = items[i].description;
+    title.style.overflowWrap = "word-break"
+    title.style.marginLeft = "47%";
+    title.style.marginBottom = "-43%"
+    images.src = items[i].image;
+    images.style.height = "180px";
+    images.style.marginTop = "23%";
+    images.style.marginBottom = "20%";
+    images.style.marginLeft = "-1%";
+    images.style.border = "4px solid chocolate";
+    images.style.borderRadius = "4px";
+    line.style.marginLeft = "47%";
+    line.style.width = "50%"
+
+    div.appendChild(names);
+    div2.appendChild(food);
+    div2.appendChild(amount);
+    div.appendChild(div2);
+    div2.appendChild(line)
+    div.appendChild(title);
+    div.appendChild(images);
+
+    content.appendChild(div)
+
+}
+
+let aLL = document.getElementById("all");
+aLL.addEventListener("click", function () {
+    content.innerHTML = '';
+    for (let i = 0; i < items.length; i++) {
+
+
+
+        const div = document.createElement('div');
+        const names = document.createElement('h1');
+        const food = document.createElement('h4');
+        const amount = document.createElement('h2');
+        const title = document.createElement('h4');
+        const images = document.createElement('img');
+
+        names.textContent = items[i].type;
+        food.textContent = items[i].name;
+        amount.textContent = items[i].price;
+        title.textContent = items[i].description;
+        images.src = items[i].image;
+
+        div.appendChild(names);
+        div.appendChild(food);
+        div.appendChild(amount);
+        div.appendChild(title);
+        div.appendChild(images);
+
+
+        content.appendChild(div);
+
+        console.log("this is the breakfast==>", content)
+    }
+})
+
+
+let breaks = document.getElementById("breakfast");
+breaks.addEventListener("click", function () {
+    let output = items.filter(ite => ite.type === "breakfast");
+    console.log("this is outpffuts==>", output)
+    content.innerHTML = '';
+    for (let i = 0; i < output.length; i++) {
+
+
+
+        const div = document.createElement('div');
+        const names = document.createElement('h1');
+        const food = document.createElement('h4');
+        const amount = document.createElement('h2');
+        const title = document.createElement('h4');
+        const images = document.createElement('img');
+
+        names.textContent = output[i].type;
+        food.textContent = output[i].name;
+        amount.textContent = output[i].price;
+        title.textContent = output[i].description;
+        images.src = output[i].image;
+
+        div.appendChild(names);
+        div.appendChild(food);
+        div.appendChild(amount);
+        div.appendChild(title);
+        div.appendChild(images);
+
+
+        content.appendChild(div);
+
+        console.log("this is the breakfast==>", content)
+    }
+})
+
+let launch = document.getElementById("launch");
+launch.addEventListener("click", function () {
+    let meal = items.filter(meals => meals.type === "launch");
+    content.innerHTML = '';
+    for (let i = 0; i < meal.length; i++) {
+
+        let div = document.createElement('div');
+        let names = document.createElement('h1');
+        let food = document.createElement('h4');
+        let amount = document.createElement('h2');
+        let title = document.createElement('h4');
+        let images = document.createElement('img');
+
+        names.textContent = meal[i].type;
+        food.textContent = meal[i].name;
+        amount.textContent = meal[i].price;
+        title.textContent = meal[i].description;
+        images.src = meal[i].image;
+
+        div.appendChild(names);
+        div.appendChild(food);
+        div.appendChild(amount);
+        div.appendChild(title);
+        div.appendChild(images);
+
+
+        content.appendChild(div);
+    }
+})
+
+
+let shakes = document.getElementById("shake");
+shakes.addEventListener("click", function () {
+    content.innerHTML = ''
+    let shake = items.filter(shakey => shakey.type === "shakes");
+    for (let i = 0; i < shake.length; i++) {
+
+        let div = document.createElement('div');
+        let names = document.createElement('h1');
+        let food = document.createElement('h4');
+        let amount = document.createElement('h2');
+        let title = document.createElement('h4');
+        let images = document.createElement('img');
+
+        names.textContent = shake[i].type;
+        food.textContent = shake[i].name;
+        amount.textContent = shake[i].price;
+        title.textContent = shake[i].description;
+        images.src = shake[i].image;
+
+        div.appendChild(names);
+        div.appendChild(food);
+        div.appendChild(amount);
+        div.appendChild(title);
+        div.appendChild(images);
+
+
+        content.appendChild(div);
+    }
+})
+
+
+let dinner = document.getElementById("dinner");
+dinner.addEventListener("click", function () {
+    content.innerHTML = ''
+    let dine = items.filter(dines => dines.type === "dinner");
+    for (let i = 0; i < dine.length; i++) {
+
+
+        let div = document.createElement('div');
+        let names = document.createElement('h1');
+        let food = document.createElement('h4');
+        let amount = document.createElement('h2');
+        let title = document.createElement('h4');
+        let images = document.createElement('img');
+
+        names.textContent = dine[i].type;
+        food.textContent = dine[i].name;
+        amount.textContent = dine[i].price;
+        title.textContent = dine[i].description;
+        images.src = dine[i].image;
+
+        div.appendChild(names);
+        div.appendChild(food);
+        div.appendChild(amount);
+        div.appendChild(title);
+        div.appendChild(images);
+
+
+        content.appendChild(div);
+    }
+});
+
+
+// let output = []
+// for (let i = 0; i < items.length; i++) {
+//     if (items[i].type==items[i].type.includes("breakfast"){
+//     // output.push()
+//     }
+// }
+
+// console.log('this is second', output)
+// let output = []
+// for (let i = 0; i < items.length; i++) {
+//     if(items[i].type===items[i+1].type){
+//         output.push(items[i].type)
+//    }
+// }
+// console.log('this is the output',output)
+
+
+// 1) to loop through the array ()
+// 2) to check for the string in the array if it already exist 
+// 3) to push the string;
+
+let result = []
+for (let i = 0; i < items.length; i++) {
+    if (!result.includes(items[i].type)) {
+        result.push(items[i].type)
+
+    }else{result.includes(items[i].type)}
+}
+console.log('this is the reuslt', result)
